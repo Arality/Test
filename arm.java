95fa9c61acc8a70c125d84720fbb93be25fa4f15
Josh Allen <arality@gmail.com>
2018-10-04 14:50:11 -0700

    Converting to Python Dev Enviroment

--- arm.java
+++
@@ -1,5 +0,0 @@
-public class arm extends robot {
-	public void move() {
-		System.out.println("I'm moving my arms bzzztt!");
-	}
-}
--- robot.java
+++
@@ -1,7 +0,0 @@
-public class robot {
-	String name;
-
-	public void speak() {
-		System.out.println("Hello 11010101 I am a Robot!");
-	}
-}
